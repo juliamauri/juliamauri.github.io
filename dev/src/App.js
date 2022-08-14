476a6471b7a5eb40c612e8dd00830174465ecf3e
Julià Mauri Costa <julimacowork@gmail.com>
2022-08-14 04:12:30 +0200

    Projects third iteration
    fixed sizing and load projects title, date and tags from json

--- dev/src/App.js
+++ dev/src/App.js
@@ -4,13 +4,13 @@ import Home from "./Home";
 import Projects from "./Projects";
 
 function App() {
-  const [page, usePage] = useState("Home");
+  const [page, setPage] = useState("Home");
   useEffect(() => {
     document.title = `${page} - Julià Mauri Costa`;
   }, [page]);
 
   switch(page){
-    case "Home": return <Home go={usePage} />;
+    case "Home": return <Home go={setPage} />;
     case "Projects": return <Projects />;
     case "About me": return <h1>About me</h1>;
     default: return <h1>Not expected to be here</h1>;
--- dev/src/Projects.js
+++ dev/src/Projects.js
@@ -1,118 +1,159 @@
-import { Box, Grid, Typography, Chip, Slider } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box, Grid, Typography, Chip, Slider, CircularProgress  } from "@mui/material";
 import ReactPlayer from "react-player";
 import { Scrollbars } from "react-custom-scrollbars-2";
 
 function valuetext(value) {
   switch (value) {
-    case 1:
+    case 0:
       return "RedEye Engine";
-    case 2:
+    case 1:
       return "Alita: Unbreakable Warrior";
-    case 3:
+    case 2:
       return "Honey Land";
-    case 4:
+    case 3:
       return "Age of Empires II - Defenders";
-    case 5:
+    case 4:
       return "Outzone Tribute";
     default:
       return "No suposed to be here";
   }
 }
 
-const badges = [
-  {id: 1, label:'ARPG'},
-  {id: 2, label:'Custom engine'},
-  {id: 3, label:'Cel Shading'}
-];
-
 function Projects() {
-  return (
-    <Box sx={{ width: "100%", height: "100vh" }}>
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=OPb1lIkBqQM"
-        width="100%"
-        height="80%"
-      />
+  const [projectsdata, setProjectsdata] = useState([]);
+  useEffect(() => {
 
-      <Box sx={{ width: "100%" }} textAlign="center">
-        <Slider
-          aria-label="Projects"
-          defaultValue={2}
-          getAriaValueText={valuetext}
-          valueLabelFormat={valuetext}
-          valueLabelDisplay="auto"
-          step={1}
-          marks
-          min={1}
-          max={5}
-          sx={{ width: "70%" }}
-          size="small"
+    const getData = async () => {
+      try {
+        const response = await fetch("./data.json");
+        const json = await response.json();
+        setProjectsdata(json);
+    } catch (error) {
+        console.log("error", error);
+    }
+    };
+
+    getData();
+  }, []);
+
+  const [projectid, setProjectid] = useState(0);
+  const handleChange = (event, newValue) => {
+    if (typeof newValue === 'number' && newValue !== projectid) {
+      setProjectid(newValue);
+    }
+  };
+
+  console.log(projectid.toString());
+  console.log(projectsdata);
+  console.log(projectsdata.length);
+  console.log(projectsdata[4]);
+
+  if(projectsdata && projectsdata.length > 0){
+    return (
+      <Box sx={{ width: "100%", height: "100vh" }}>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=OPb1lIkBqQM"
+          width="100%"
+          height="80%"
         />
-      </Box>
-
-      <Grid
-        container
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="stretch"
-        height="16vh"
-        px={1}
-      >
-        <Grid item xs={4}>
-
+  
+        <Box sx={{ width: "100%", height: "3vh" }} textAlign="center">
+          <Slider
+            aria-label="Projects"
+            defaultValue={0}
+            getAriaValueText={valuetext}
+            valueLabelFormat={valuetext}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+            min={0}
+            max={4}
+            sx={{ width: "70%" }}
+            size="small"
+          />
+        </Box>
+  
         <Grid
-        container
-        direction="column"
-        height="16vh"
-        mr={1}
-      >
-                <Grid item xs={3}>
-
-          <Typography component="div" variant="h5">
-            Alita: Unbreakable Warrior
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="text.secondary"
-            component="div"
-          >
-            11 FEB 2019 – 6 JUN 2019
-          </Typography>
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="stretch"
+          height="17vh"
+          px={0.7}
+        >
+          <Grid item xs={4}>
+            <Grid container direction="column" height="17vh" pr={0.2}>
+              <Grid item xs={3}>
+                <Typography
+                  component="div"
+                  variant="h5"
+                  style={{ height: "4.5vh" }}
+                >
+                  {projectsdata[projectid].title}
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                  style={{ height: "4vh" }}
+                >
+                  {projectsdata[projectid].date}
+                </Typography>
+              </Grid>
+  
+              <Grid item xs={6} sx={{ textAlign: "center" }}>
+                <Scrollbars style={{ width: "95%", height: "8.5vh" }}>
+                  {projectsdata[projectid].tags.map(({ id, tag }) => {
+                    return (
+                      <Chip
+                        key={id}
+                        label={tag}
+                        color="primary"
+                        size="small"
+                        sx={{ m: 0.2 }}
+                      />
+                    );
+                  })}
+                </Scrollbars>
+              </Grid>
+            </Grid>
           </Grid>
-
-          <Grid item xs={6} sx={{textAlign:'center'}}>
-
-          <Scrollbars style={{ width: "95%", height: "100%" }}>
-          {badges.map(({id, label}) =>{
-            return (<Chip key={id} label={label} color="primary" size="small" sx={{m:0.2}} />)
-          })}
-          </Scrollbars> 
+          <Grid item xs={8}>
+            <Scrollbars
+              style={{
+                width: "100%",
+                height: "100%",
+                textAlign: "justify"
+              }}
+            >
+              <Typography component="div" variant="body1">
+                Alita: Unbreakable Warrior is an RPG action game that will let you
+                play as Alita, a female cyborg, as you explore the world and
+                encounter a wide variety of enemies through different scenarios.
+                The events in the game follow the story of the movie Alita: Battle
+                Angel. JellyBit is the team behind Alita Unbreakable Warrior. We
+                are a small group of 18 students from the Polytechnic University
+                of Catalonia (UPC), currently in the third year of the Bachelor’s
+                Degree in Video Game Design and Development. Based in Terrassa,
+                Barcelona, our goal is to simulate an indie studio and develop a
+                game within three months for the Project III subject. This is our
+                first time working all together, so we expect to learn a lot from
+                each other - as professionals and as individuals - and grow - as
+                individuals and as a group. We are team players!{" "}
+              </Typography>
+            </Scrollbars>
           </Grid>
-          </Grid>
-
         </Grid>
-        <Grid item xs={8}>
-          <Scrollbars style={{ width: "100%", height: "100%", textAlign: "justify" }}>
-            <Typography component="div" variant="body1">
-              Alita: Unbreakable Warrior is an RPG action game that will let you
-              play as Alita, a female cyborg, as you explore the world and
-              encounter a wide variety of enemies through different scenarios.
-              The events in the game follow the story of the movie Alita: Battle
-              Angel. JellyBit is the team behind Alita Unbreakable Warrior. We
-              are a small group of 18 students from the Polytechnic University
-              of Catalonia (UPC), currently in the third year of the Bachelor’s
-              Degree in Video Game Design and Development. Based in Terrassa,
-              Barcelona, our goal is to simulate an indie studio and develop a
-              game within three months for the Project III subject. This is our
-              first time working all together, so we expect to learn a lot from
-              each other - as professionals and as individuals - and grow - as
-              individuals and as a group. We are team players!{" "}
-            </Typography>
-          </Scrollbars>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+      </Box>
+    );
+  }
+  else
+  {
+    return (<CircularProgress color="primary" />);
+  }
+
 }
 
 export default Projects;
